77c9ef864f6150df5fa207cf657b578514fb96b5
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-25 13:39:48 -0500

    WEB

--- src/main/java/VO/InteractionFlow.java
+++ src/main/java/VO/InteractionFlow.java
@@ -10,5 +10,54 @@ public abstract class InteractionFlow
     private String name;
     private int id;
     public String typeEvent;
-    
+    private ViewElement interactionFlow;
+
+    public InteractionFlow()
+    {
+        ParameterBindingGroup parameterBindingGroup = new ParameterBindingGroup();
+    }
+            
+    public InteractionFlow(String name, int id, String typeEvent, ViewElement interactionFlow) {
+        this.name = name;
+        this.id = id;
+        this.typeEvent = typeEvent;
+        this.interactionFlow = interactionFlow;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTypeEvent() {
+        return typeEvent;
+    }
+
+    public void setTypeEvent(String typeEvent) {
+        this.typeEvent = typeEvent;
+    }
+
+    public ViewElement getInteractionFlow() {
+        return interactionFlow;
+    }
+
+    public void setInteractionFlow(ViewElement interactionFlow) {
+        this.interactionFlow = interactionFlow;
+    }
+
+    @Override
+    public String toString() {
+        return "InteractionFlow{" + "name=" + name + ", id=" + id + ", typeEvent=" + typeEvent + ", interactionFlow=" + interactionFlow + '}';
+    }
 }
