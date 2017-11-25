5fe26e1da2f1d8a777e27d5a0c18007b22875f05
BrianS01 <brian.sterling@correo.usa.edu.co>
2017-11-25 13:20:34 -0500

    WEB

--- src/main/java/VO/Parameters.java
+++ src/main/java/VO/Parameters.java
@@ -5,11 +5,45 @@
  */
 package VO;
 
-/**
- *
- * @author BRIAN
- */
 public class Parameters
 {
-    
+    public int id;
+    private ParameterBinding target;
+    private ParameterBinding source;
+
+    public int getId()
+    {
+        return id;
+    }
+
+    public void setId(int id)
+    {
+        this.id = id;
+    }
+
+    public ParameterBinding getTarget()
+    {
+        return target;
+    }
+
+    public void setTarget(ParameterBinding target)
+    {
+        this.target = target;
+    }
+
+    public ParameterBinding getSource()
+    {
+        return source;
+    }
+
+    public void setSource(ParameterBinding source)
+    {
+        this.source = source;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Parameters{" + "id=" + id + ", target=" + target + ", source=" + source + '}';
+    }
 }
